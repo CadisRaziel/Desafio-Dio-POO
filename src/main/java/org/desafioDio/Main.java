cf6977dab1a604dd64649571768d3f998d7542c5
vitu <vitorcsharp@hotmail.com>
2023-03-10 14:31:46 -0300

    concept of polymorphism

--- src/main/java/org/desafioDio/Main.java
+++ src/main/java/org/desafioDio/Main.java
@@ -1,12 +1,19 @@
 package org.desafioDio;
 
+import org.desafioDio.Dominio.Conteudo;
 import org.desafioDio.Dominio.Curso;
 import org.desafioDio.Dominio.Mentoria;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.LinkedList;
 
 public class Main {
+
+    //Herança
+    //tudo que tem dentro de conteudo tem em curso/mentoria
+    //mais nem tudo que tem em curso/mentoria tem dentro de conteudo
+
     public static void main(String[] args) {
         Curso curso1 = new Curso();
         curso1.setTitulo("Curso java");
@@ -21,5 +28,10 @@ public class Main {
         mentoria.setData(LocalDate.now());
 
         System.out.println(mentoria);
+
+        //Polimorfismo
+        //Conteudo conteudo = new Curso();
+        //Conteudo conteudo1 = new Mentoria();
+        //List<String> lista1 = new ArrayList<>();
     }
 }
