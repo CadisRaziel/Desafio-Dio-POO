3d086181c43918d829efd63c5495fb65529b3d26
vitu <vitorcsharp@hotmail.com>
2023-03-10 15:32:25 -0300

    project done

--- src/main/java/org/desafioDio/Dominio/Dev.java
+++ src/main/java/org/desafioDio/Dominio/Dev.java
@@ -2,6 +2,7 @@ package org.desafioDio.Dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -11,9 +12,39 @@ public class Dev {
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
-    public void inscreverBootcamp(Bootcamp bootcamp) {}
-    public void progredit(){}
-    public void calcularXp(){}
+    public void inscreverBootcamp(Bootcamp bootcamp) {
+        //A partir do momento que o dev se inscreve no bootcamp todo o conteudo do bootcamp vai pra sua conta em 'conteudosInscritos'
+        //resumindo eu to pegando tudo que tem dentro do bootcamp usando getConteudos e colocando dentro de conteudosINscritos
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+
+        //adicionando o dev no bootcamp
+        bootcamp.getDevsIncritos().add(this); //this -> se refere a classe 'Dev'
+    }
+
+    public void progredit() {
+        //findFirst -> para pegar na ordem, o primeiro que ele for fazendo
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+
+        //agora preciso tirar do conteudoInscritos e colocar no conteudosConcluidos
+        if (conteudo.isPresent()) {
+            //se existir eu coloco dentro de conteudosConcluidos
+            this.conteudosConcluidos.add(conteudo.get());
+
+            //removendo do conteudos incritos
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+            //caso esteja vazio
+            System.err.println("Você não esta matriculado em nenhum conteudo");
+        }
+    }
+
+    public double calcularXp() {
+        return this.conteudosConcluidos
+                .stream()
+                //.mapToDouble(conteudo -> conteudo.calcularXp()) // ou posso por lambda ou usa o reference method como abaixo
+                .mapToDouble(Conteudo::calcularXp)
+                .sum(); //sum -> somar
+    }
 
 
     public String getNome() {
--- src/main/java/org/desafioDio/Main.java
+++ src/main/java/org/desafioDio/Main.java
@@ -1,8 +1,6 @@
 package org.desafioDio;
 
-import org.desafioDio.Dominio.Conteudo;
-import org.desafioDio.Dominio.Curso;
-import org.desafioDio.Dominio.Mentoria;
+import org.desafioDio.Dominio.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -20,18 +18,55 @@ public class Main {
         curso1.setDescricao("descrição curso java");
         curso1.setCargaHoraria(8);
 
-        System.out.println(curso1);
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso mysql");
+        curso2.setDescricao("descrição curso mysql");
+        curso2.setCargaHoraria(6);
+
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de java");
         mentoria.setDescricao("descrição da mentoria de java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(mentoria);
+
 
         //Polimorfismo
         //Conteudo conteudo = new Curso();
         //Conteudo conteudo1 = new Mentoria();
         //List<String> lista1 = new ArrayList<>();
+
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devVitor = new Dev();
+        devVitor.setNome("Vitor");
+        devVitor.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos Vitor -> " + devVitor.getConteudosInscritos());
+        System.out.println();
+        //fazendo o dev se concluir em um curso
+        devVitor.progredit();
+        //conteudo que eu ja conclui
+        System.out.println("Conteudos inscritos Vitor -> " + devVitor.getConteudosInscritos());
+        System.out.println();
+        System.out.println("Conteudos concluidos Vitor -> " + devVitor.getConteudosConcluidos());
+        System.out.println();
+        System.out.println("XP: -> " + devVitor.calcularXp());
+        System.out.println("----------------------");
+
+        Dev devAri = new Dev();
+        devAri.setNome("Ariella");
+        devAri.inscreverBootcamp(bootcamp);
+        System.out.println();
+        System.out.println("Conteudos inscritos Ari -> " + devAri.getConteudosInscritos());
+        System.out.println();
+        System.out.println("Conteudos concluidos Ari -> " + devAri.getConteudosConcluidos());
+
+
     }
 }
